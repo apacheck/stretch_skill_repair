26d4866a0dd734b237b46ecc26d061b442a7aa91
apacheck <akp84@cornell.edu>
2022-06-23 17:13:48 -0400

    move code that moves stretch to funcs

--- nodes/StretchSkill.py
+++ nodes/StretchSkill.py
@@ -37,6 +37,13 @@ import moveit_msgs.msg
 from moveit_commander.conversions import pose_to_list
 from synthesis_based_repair.tools import write_spec, clear_file, dict_to_formula, json_load_wrapper
 
+from dl2_lfd.nns.dmp_nn import DMPNN
+from dl2_lfd.dmps.dmp import load_dmp_demos, DMP
+import torch
+from dl2_lfd.helper_funcs.conversions import np_to_pgpu
+
+DEVICE="cpu"
+
 # import stretch_funmap.navigate as nv
 
 IS_SIM = True
@@ -44,9 +51,9 @@ if IS_SIM:
     ORIGIN_FRAME = 'odom'
     STRETCH_FRAME = 'robot::base_link'
     CMD_VEL_TOPIC = '/stretch_diff_drive_controller/cmd_vel'
-    DO_THETA_CORRECTION = True
-    DO_MOVE_ARM = True
-    DO_MOVE_GRIPPER = True
+    DO_THETA_CORRECTION = False
+    DO_MOVE_ARM = False
+    DO_MOVE_GRIPPER = False
 else:
     IS_SIM = False
     ORIGIN_FRAME = 'origin'
@@ -62,6 +69,9 @@ class StretchSkill(hm.HelloNode):
         if IS_SIM:
             moveit_commander.roscpp_initialize(sys.argv)
             rospy.init_node('controller', anonymous=True)
+            robot = moveit_commander.RobotCommander()
+            # rospy.sleep(10.0)
+            self.move_group_arm = moveit_commander.MoveGroupCommander("stretch_arm")
         else:
             hm.HelloNode.__init__(self)
             hm.HelloNode.main(self, 'move_gripper', 'move_gripper', wait_for_first_pointcloud=False)
@@ -80,7 +90,6 @@ class StretchSkill(hm.HelloNode):
 
             with self.move_lock:
                 self.handover_goal_ready = False
-
 
     def open_gripper(self):
         if DO_MOVE_GRIPPER:
@@ -127,6 +136,7 @@ class StretchSkill(hm.HelloNode):
             rospy.loginfo("Global parameter set to not move gripper")
 
     def findPose(self, frame):
+        #TODO Change this to not be a class function, but pass in the transform or something?
         found_transform = False
         cnt = 1
         while not found_transform:
@@ -144,140 +154,134 @@ class StretchSkill(hm.HelloNode):
         return trans
 
     def getJointValues(self):
+        # Returns extension, lift, wrist yaw
         if IS_SIM:
             move_group_arm = moveit_commander.MoveGroupCommander("stretch_arm")
             joint_values = move_group_arm.get_current_joint_values()
         else:
             rospy.loginfo("Not implemented")
-        return joint_values
+        return np.array([np.sum(joint_values[1:5]), joint_values[0], joint_values[5]])
 
     def follow_trajectory(self, data):
         # Data should be a numpy array with x, y, theta, wrist_extension, z, wrist_theta
         rospy.loginfo("Starting follow_trajectory")
-        if IS_SIM:
-            robot = moveit_commander.RobotCommander()
-            # rospy.sleep(10.0)
-            move_group_arm = moveit_commander.MoveGroupCommander("stretch_arm")
-
-        close_enough = 0.01
-        epsilon = 0.1
-        maxV = 2
-        wheel2center = 0.1778
 
         for d in data:
 
-            dl = d.tolist()
             if DO_MOVE_ARM:
-                rospy.loginfo("Moving gripper and arm to: extension: {:.2f}, lift: {:.2f}, rotation: {:.2f}".format(d[3], d[4], d[5]))
-                if not IS_SIM:
-                    with self.move_lock:
-                        pose = dict()
-                        if d[3] != -10:
-                            pose['wrist_extension'] = d[3]
-                        if d[4] != -10:
-                            pose['joint_lift'] = d[4]
-                        if d[5] != -10:
-                            pose['joint_wrist_yaw'] = d[5]
-                        # 'rotate_mobile_base': 0.1
-                        # 'translate_mobile_base': -0.2
+                self.moveArm(d[3:])
 
-                        # The base can also be controlled with odometery?
-                        # translate_mobile_base
-                        # rotate_mobile_base
-                        self.move_to_pose(pose)
-                else:
-                    joint_goal = move_group_arm.get_current_joint_values()
-                    if dl[4] != -10:
-                        joint_goal[0] = dl[4]
-                    if dl[3] != -10:
-                        joint_goal[1] = dl[3]/4
-                        joint_goal[2] = dl[3]/4
-                        joint_goal[3] = dl[3]/4
-                        joint_goal[4] = dl[3]/4
-                    if dl[5] != -10:
-                        joint_goal[5] = dl[5]
+            if d[0] != -10:
+                self.visitWaypoint(d[:3])
 
-                    move_group_arm.go(joint_goal, wait=True)
+            if DO_THETA_CORRECTION and d[2] != -10:
+                self.rotateToTheta(d[2])
 
-            rospy.loginfo("Moving base to: x: {:.2f}, y: {:.2f}, theta: {:.2f}".format(d[0], d[1], d[2]))
-
-            # Control the mobile base with feedback linearization and visiting waypoint
-            # rospy.loginfo("Beginning feedback linearization for visit waypoint")
-            at_waypoint = False
-            if d[0] == -10:
-                at_waypoint = True
-            while not at_waypoint:
-                trans_stretch = self.findPose(STRETCH_FRAME)
-
-                dist_to_waypoint = np.sqrt([np.square(d[0] - trans_stretch.translation.x) +
-                                            np.square(d[1] - trans_stretch.translation.y)])
-                # rospy.loginfo("Distance to waypoint: {}".format(dist_to_waypoint))
-
-                if dist_to_waypoint < close_enough:
-                    at_waypoint = True
-
-                if not at_waypoint:
-                    cmd_vx, cmd_vy, theta = findDeltaPose(trans_stretch, d)
-                    cmd_v, cmd_w = feedbackLin(cmd_vx, cmd_vy, theta, epsilon)
-                    cmd_v, cmd_w = thresholdVel(cmd_v, cmd_w, maxV, wheel2center)
-                    # _, cmd_v, cmd_w = calc_control_command(cmd_vx, cmd_vy, theta, epsilon)
-
-                    # rospy.loginfo("Robot is at: x: {:.3f}, y: {:.3f}, z: {:.3f}, theta: {:.3f}".format(trans_stretch.translation.x, trans_stretch.translation.y, trans_stretch.translation.z, theta))
-                    # rospy.loginfo("cmd_v: {} cmd_w: {}".format(cmd_v, cmd_w))
-
-                    vel_msg = Twist()
-                    vel_msg.linear.x = cmd_v
-                    vel_msg.angular.z = cmd_w
-                    self.vel_pub.publish(vel_msg)
-                    self.rate.sleep()
-
-            # Rotate if the current and desired theta do not match
-            # rospy.loginfo("Beginning theta correction")
-            # rospy.loginfo("Goal theta: {}".format(d[2]))
-            make_theta_correction = True
-            while make_theta_correction and DO_THETA_CORRECTION and d[2] != -10:
-                trans_stretch = self.findPose(STRETCH_FRAME)
-
-                _, _, theta = findDeltaPose(trans_stretch, d)
-                # theta += np.pi
-                # print("Real theta: {}".format(theta))
-                # if theta < np.pi:
-                #     theta += 2 * np.pi
-                # if theta > np.pi:
-                #     theta -= 2 * np.pi
-                # rospy.loginfo("Robot is at: x: {:.3f}, y: {:.3f}, z: {:.3f}, theta: {:.3f}".format(trans_stretch.translation.x, trans_stretch.translation.y, trans_stretch.translation.z, theta))
-
-                cmd_w = d[2] - theta
-                if cmd_w > np.pi:
-                    cmd_w -= 2 * np.pi
-                if cmd_w < -np.pi:
-                    cmd_w += 2 * np.pi
-                if np.abs(cmd_w) < 0.001:
-                    make_theta_correction = False
-                else:
-                    # rospy.loginfo("cmd_v: {} cmd_w: {}".format(0, cmd_w))
-                    vel_msg = Twist()
-                    vel_msg.angular.z = cmd_w
-                    self.vel_pub.publish(vel_msg)
-                    self.rate.sleep()
-
-            rospy.loginfo("Verifying new pose")
             trans_stretch = self.findPose(STRETCH_FRAME)
-            _, _, theta = findDeltaPose(trans_stretch, d)
+            theta = findTheta(trans_stretch, d)
             found_transform = False
             trans_stretch = self.findPose(STRETCH_FRAME)
 
             rospy.loginfo("Robot is at: x: {:.3f}, y: {:.3f}, z: {:.3f}, theta: {:.3f}".format(trans_stretch.translation.x, trans_stretch.translation.y, trans_stretch.translation.z, theta))
 
-            trans_ee_l = self.findPose('robot::link_gripper_finger_left')
-            trans_ee_r = self.findPose('robot::link_gripper_finger_right')
-
-            rospy.loginfo("Gripper left is at: x: {:.3f}, y: {:.3f}, z: {:.3f}".format(trans_ee_l.translation.x, trans_ee_l.translation.y, trans_ee_l.translation.z))
-            rospy.loginfo("Gripper right is at: x: {:.3f}, y: {:.3f}, z: {:.3f}".format(trans_ee_r.translation.x, trans_ee_r.translation.y, trans_ee_r.translation.z))
-
         rospy.loginfo("Completed follow_trajectory")
+
         if IS_SIM and DO_MOVE_ARM:
-            move_group_arm.stop()
+            self.move_group_arm.stop()
+
+    def rotateToTheta(self, arg_goal_theta, arg_close_enough=0.01):
+        make_theta_correction = True
+        while make_theta_correction:
+            trans_stretch = self.findPose(STRETCH_FRAME)
+            theta = findTheta(trans_stretch, d)
+            cmd_w = arg_goal_theta - theta
+            if cmd_w > np.pi:
+                cmd_w -= 2 * np.pi
+            if cmd_w < -np.pi:
+                cmd_w += 2 * np.pi
+            if np.abs(cmd_w) < arg_close_enough:
+                make_theta_correction = False
+            else:
+                # rospy.loginfo("cmd_v: {} cmd_w: {}".format(0, cmd_w))
+                vel_msg = Twist()
+                vel_msg.angular.z = cmd_w
+                self.vel_pub.publish(vel_msg)
+                self.rate.sleep()
+
+        return True
+
+    def visitWaypoint(self, waypoint_xytheta, arg_close_enough=0.01, arg_epsilon=0.1, arg_maxV=2, arg_wheel2center=0.1778):
+        rospy.loginfo("Moving base to: x: {:.2f}, y: {:.2f}, theta: {:.2f}".format(waypoint_xytheta[0], waypoint_xytheta[1], waypoint_xytheta[2]))
+        at_waypoint = False
+        while not at_waypoint:
+            trans_stretch = self.findPose(STRETCH_FRAME)
+
+            dist_to_waypoint = np.sqrt([np.square(waypoint_xytheta[0] - trans_stretch.translation.x) +
+                                        np.square(waypoint_xytheta[1] - trans_stretch.translation.y)])
+            # rospy.loginfo("Distance to waypoint: {}".format(dist_to_waypoint))
+
+            if dist_to_waypoint < arg_close_enough:
+                at_waypoint = True
+
+            if not at_waypoint:
+                cmd_vx, cmd_vy, theta = findCommands(trans_stretch, d)
+                cmd_v, cmd_w = feedbackLin(cmd_vx, cmd_vy, theta, epsilon)
+                # cmd_v, cmd_w = thresholdVel(cmd_v, cmd_w, maxV, wheel2center)
+                # _, cmd_v, cmd_w = calc_control_command(cmd_vx, cmd_vy, theta, epsilon)
+
+                # rospy.loginfo("Robot is at: x: {:.3f}, y: {:.3f}, z: {:.3f}, theta: {:.3f}".format(trans_stretch.translation.x, trans_stretch.translation.y, trans_stretch.translation.z, theta))
+                # rospy.loginfo("cmd_v: {} cmd_w: {}".format(cmd_v, cmd_w))
+
+                vel_msg = Twist()
+                vel_msg.linear.x = cmd_v
+                vel_msg.angular.z = cmd_w
+                self.vel_pub.publish(vel_msg)
+                self.rate.sleep()
+
+        return True
+
+    def moveArm(self, arg_desired_ext_lift_yaw):
+        dl = arg_desired_ext_lift_yaw.tolist()
+        rospy.loginfo("Moving gripper and arm to: extension: {:.2f}, lift: {:.2f}, rotation: {:.2f}".format(arg_desired_ext_lift_yaw[0], arg_desired_ext_lift_yaw[1], arg_desired_ext_lift_yaw[2]))
+        if not IS_SIM:
+            with self.move_lock:
+                pose = dict()
+                if arg_desired_ext_lift_yaw[0] != -10:
+                    pose['wrist_extension'] = arg_desired_ext_lift_yaw[0]
+                if arg_desired_ext_lift_yaw[1] != -10:
+                    pose['joint_lift'] = arg_desired_ext_lift_yaw[1]
+                if arg_desired_ext_lift_yaw[2] != -10:
+                    pose['joint_wrist_yaw'] = arg_desired_ext_lift_yaw[2]
+                # 'rotate_mobile_base': 0.1
+                # 'translate_mobile_base': -0.2
+
+                # The base can also be controlled with odometery?
+                # translate_mobile_base
+                # rotate_mobile_base
+                self.move_to_pose(pose)
+        else:
+            joint_goal = self.move_group_arm.get_current_joint_values()
+            if dl[1] != -10:
+                if dl[1] < 0:
+                    dl[1] = 0
+                if dl[1] > 1:
+                    dl[1] = 1
+                joint_goal[0] = dl[4]
+            if dl[0] != -10:
+                if dl[0] < 0:
+                    dl[0] = 0
+                if dl[0] > 0.5:
+                    dl[0] = 0.5
+                joint_goal[1] = dl[0]/4
+                joint_goal[2] = dl[0]/4
+                joint_goal[3] = dl[0]/4
+                joint_goal[4] = dl[0]/4
+            if dl[2] != -10:
+                joint_goal[5] = dl[2]
+            # rospy.loginfo("Joint goal {}".format(joint_goal))
+            self.move_group_arm.go(joint_goal, wait=True)
+
+        return True
 
 def feedbackLin(arg_cmd_vx, arg_cmd_vy, arg_theta, arg_epsilon):
     cmd_vi = np.array([[arg_cmd_vx], [arg_cmd_vy]])
@@ -299,8 +303,7 @@ def thresholdVel(arg_cmd_v, arg_cmd_w, arg_maxV, arg_wheel2center):
 
     return arg_cmd_v, arg_cmd_w
 
-
-def findDeltaPose(arg_cur_pose, arg_desired_pose):
+def findCommands(arg_cur_pose, arg_desired_pose):
     cmd_vx = arg_desired_pose[0] - arg_cur_pose.translation.x
     cmd_vy = arg_desired_pose[1] - arg_cur_pose.translation.y
     q1 = arg_cur_pose.rotation.x
@@ -312,17 +315,61 @@ def findDeltaPose(arg_cur_pose, arg_desired_pose):
         theta -= np.pi
     return cmd_vx, cmd_vy, theta
 
+def findTheta(arg_cur_pose):
+    q1 = arg_cur_pose.rotation.x
+    q2 = arg_cur_pose.rotation.y
+    q3 = arg_cur_pose.rotation.z
+    q0 = arg_cur_pose.rotation.w
+    theta = np.arctan2(2 * (q1 * q2 + q0 * q3), q0 ** 2 + q1 ** 2 - q2 ** 2 - q3 **2)
+    if not IS_SIM:
+        theta -= np.pi
+    return theta
+
 
 def findTrajectoryFromDMP(start_pose, end_pose, skill_name, dmp_folder, opts):
     model = DMPNN(opts['start_dimension'], 1024, opts['dimension'], opts['basis_fs']).to(DEVICE)
     model.load_state_dict(torch.load(dmp_folder + skill_name + ".pt"))
-    learned_weights = model(np_to_pgpu(start_pose))
+    starts = np.zeros([1, 2, start_pose.size])
+    starts[0, 0, :] = start_pose
+    starts[0, 1, :] = end_pose
+    rospy.loginfo("Starts {}".format(starts))
+    learned_weights = model(np_to_pgpu(starts))
     dmp = DMP(opts['basis_fs'], opts['dt'], opts['dimension'])
     # print("Calculating rollout")
     learned_rollouts, _, _ = \
-        dmp.rollout_torch(torch.tensor(start_pose).to(DEVICE), torch.tensor(start_pose).to(DEVICE), learned_weights)
-    print(learned_rollouts)
-    return learned_rollouts
+        dmp.rollout_torch(torch.tensor(starts[:, 0, :]).to(DEVICE), torch.tensor(starts[:, 1, :]).to(DEVICE), learned_weights)
+    out = learned_rollouts[0, :, :].cpu().detach().numpy()
+
+    out = np.vstack([out, end_pose])
+    rospy.loginfo("Learned rollout {}".format(out))
+
+    return out
+
+
+def findObjectPickupPose(obj_pose, obj_name):
+    stretch_pose = -10 * np.ones([1, 6])
+    if block_name == "box_1":
+        stretch_pose[0] = obj_pose.translation.x - 0.04
+        stretch_pose[1] = obj_pose.translation.y - 0.75
+        stretch_pose[2] = np.pi
+    elif block_name == "box_2":
+        stretch_pose[0] = obj_pose.translation.x - 0.75
+        stretch_pose[1] = obj_pose.translation.y - 0.04
+        stretch_pose[2] = 3 * np.pi / 2
+    elif block_name == "box_3":
+        stretch_pose[0] = obj_pose.translation.x + 0.04
+        stretch_pose[1] = obj_pose.translation.y + 0.75
+        stretch_pose[2] = 0
+
+    return stretch_pose
+
+
+def addJointValuesToPose(arg_stretch_pose, arg_joint_values):
+    arg_stretch_pose[3] = arg_joint_values[0]
+    arg_stretch_pose[4] = arg_joint_values[1]
+    arg_stretch_pose[5] = arg_joint_values[2]
+
+    return arg_stretch_pose
 
 
 if __name__ == '__main__':
@@ -334,14 +381,28 @@ if __name__ == '__main__':
         args, unknown = parser.parse_known_args()
         node = StretchSkill()
 
-        unit_box_pose = node.findPose('unit_box::link')
-        start_pose = node.getJointValues()
-        end_pose = np.array([unit_box_pose.translation.x-.04, unit_box_pose.translation.y - 0.75, np.pi, -10, -10, -10])
-        stretch_base_box_pickup[3:] = start_pose[3:]
+        # Find where the robot is, where it should pickup the block, and how it should get between them
+        unit_box_pose = node.findPose('box_2::link')
+        base_link = node.findPose('robot::base_link')
+        start_pose = np.array([base_link.translation.x, base_link.translation.y, 0, -10, -10, -10])
+        end_pose = findObjectPickupPose(unit_box_pose, 'box_2')
+        jv = node.getJointValues()
+        start_pose = addJointValuesToPose(start_pose, jv)
+        end_pose = addJointValuesToPose(end_pose, jv)
+
+        rospy.loginfo("Startpose: {}".format(start_pose))
+        rospy.loginfo("End pose: {}".format(end_pose))
+
         node.open_gripper()
         rospy.loginfo("Moving to box")
-        stretch_base_traj = findTrajectoryFromDMP(start_pose, end_pose, 'stretchSkill3to1', folder_dmps, dmp_opts)
+        stretch_base_traj = findTrajectoryFromDMP(start_pose, end_pose, 'skillStretch1to2', folder_dmps, dmp_opts)
         node.follow_trajectory(stretch_base_traj)
+
+        # correct final pose
+        node.rotateToTheta(end_pose[2])
+
+        # Reach the block
+
 
         # rospy.loginfo("Lifting arm")
         # ee_left = node.findPose('robot::link_gripper_finger_left')
